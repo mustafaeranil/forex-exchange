b475e869a4ae13bfebf8fb44f11daf65f70a02b0
Mustafa ERANIL <mustafaeranil2@gmail.com>
2022-09-14 13:48:35 +0300

    feat: add icon component

---
+++ src/components/Icon.vue
@@ -0,0 +1,26 @@
+<template>
+  <inline-svg :src="dynamicIcon"></inline-svg>
+</template>
+<script>
+import InlineSvg from "vue-inline-svg"
+
+export default {
+  name: "svgIcon",
+  components: {
+    InlineSvg
+  },
+  props: {
+    name: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    dynamicIcon() {
+      return require(`@/assets/icons/${this.name}.svg`)
+    }
+  }
+}
+</script>
+
+<style></style>
